665c4d3d25fdf3e1efdf041eb9e482d311bd4a19
Ann Likowo Molinge <ann.molinge@gmail.com>
2023-07-19 19:16:13 -0500

    Update to CSS

--- script.js
+++ script.js
@@ -2,20 +2,20 @@
 
 // QuerySelector: Navigates to an HTML tag ( The DOM) by the class name and targets it.
 
-// For this Calculator App,First, make the clear screen button functional, so you can use it to clear the screen when you click other buttons. NB: Codes read from top to bottom chronologically.What ever code you write at the beging of your coding, will be read first and will affect subsequent codes if needed within the new code.
+// For this Calculator App,First, make the clear screen button functional, so you can use it to clear the screen when you click other buttons. NB: Codes read from top to bottom chronologically.What ever code you write at the beging of your coding, will be read first and can affect subsequent codes if needed.
 
 // How to make the Clear (CLR) button functional to clear screenBar
     //1) Select the element from the DOM using querryselector
 let screenBar = document.querySelector('.screenBar')
     // Declare a function  that will make the CLR button functional when clicked. Use the Arrow Function , the .innerHTML property to go in to the HTML tag and change(update) it's content to the new data you assign it to, and the HTML onClick() method to make the button clickable.
 const clearScreenBar = () => {
-  screenBar.innerHTML = "";
+ screenBar.innerHTML = ""; 
 }
 
 // Once clicked/initialized --- I want the num key to show its value to the screen. Use the append() method to make the numbers appear on the screen. Pass number as both the parameter and argument.
 const upLoadToScreen = (number) => {
     screenBar.innerHTML = "";
-    screenBar.append(number)
+    screenBar.append(number)   
 }
 
 
